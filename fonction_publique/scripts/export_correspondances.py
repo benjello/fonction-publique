9fe4b22a97aa338ac134c3f0176d8847bdf5fcef
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-03-23 17:40:04 +0100

    Improve export_correspondances (add unslugified libemploi)

--- fonction_publique/scripts/export_correspondances.py
+++ fonction_publique/scripts/export_correspondances.py
@@ -27,7 +27,6 @@ def main():
     parser.add_argument('-t', '--target',
         default = os.path.join(libelles_emploi_directory, 'correspondances.csv'),
         help = 'path of the conversion result i.e. csv file')
-    # parser.add_argument('--format', default = 'csv', choices = ['csv', 'xls'], help = 'target file format')
     parser.add_argument('--force', action = 'store_true', default = False, help = "force overwrite ")
     parser.add_argument('--separator', default = ';', choices = [',', ';'], help = 'separator (csv only)')
     parser.add_argument('--decimal', default = '.', choices = [',', '.'], help = 'decimal mark (csv only)')
@@ -37,21 +36,23 @@ def main():
     logging.basicConfig(level = logging.INFO if args.verbose else logging.WARNING, stream = sys.stdout)
 
     assert args.separator != args.decimal, "Separator and decimal mark cannot be the same: {}".args.decimal
-    df = pd.read_hdf(correspondance_data_frame_path)
-    # if args.format == 'csv':
-    assert args.target.endswith('.csv'), "Target file name should end with '.csv'"
-    # else:
-    #     assert args.target.endswith('.xls'), "Target file name should end with '.xls'"
+    log.info('Reading data in {}'.format(args.source))
+    correspondances = pd.read_hdf(args.source).rename(columns = dict(libelle = 'libemploi_slugified'))
+    correspondance_libemploi_slug_h5 = os.path.join(libelles_emploi_directory, 'correspondance_libemploi_slug.h5')
+    correspondance_libemploi_slug = (pd.read_hdf(correspondance_libemploi_slug_h5, 'correspondance_libemploi_slug')
+        [['libemploi', 'libemploi_slugified']]
+        .drop_duplicates()
+        )
+    output = correspondances.merge(correspondance_libemploi_slug).head(10)
+    log.info('Merging with {} to {}'.format(args.source, args.target))
 
-    log.info('Start converting data in {} to {}'.format(args.source, args.target))
+    assert args.target.endswith('.csv'), "Target file name should end with '.csv'"
+    log.info('Start converting to {}'.format(args.source, args.target))
 
     if os.path.exists(args.target) and not args.force:
         log.warn("{} already exists.\nUse -t option to specify a new file or use the --force option to overwrite existing file".format(args.target))
     else:
-    # if args.format == 'csv':
-        df.to_csv(args.target, sep = args.separator, decimal = args.decimal, index = False)
-    # else:
-    #     df.to_excel(args.target)
+        output.to_csv(args.target, sep = args.separator, decimal = args.decimal, index = False)
 
 
 if __name__ == "__main__":
